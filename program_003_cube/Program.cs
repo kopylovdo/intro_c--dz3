9a8279e2066682b8cb200f7975f985d82d08ba2a
Дмитрий Копылов <dkopylov@MacBook-Air-2.local>
2022-09-04 21:39:35 +0300

    Task3 init commit

---
+++ program_003_cube/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N
+
+//принимаем чесло на вход
+Console.Write("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+//объявляем массив
+int[] cube = new int[N];
+
+int index = 0;
+
+//заполняем массив значениями кубов индекса каждого элемента от 1 до N
+while (index < N)
+{
+    cube[index] = (index + 1) * (index + 1) * (index + 1);
+    //выводим на экран каждый элемент получающегося массива
+    Console.Write($"{cube[index]} ");
+    index++;
+}
+Console.WriteLine();
